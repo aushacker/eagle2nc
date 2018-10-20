e51272ae1ab3865f2a5bc3664c580eb27b8293f5
Stephen Davies <sjdavies2@icloud.com>
2018-10-20 18:52:24 +1100

    More unit tests.

---
+++ src/test/java/com/github/aushacker/eagle2nc/xml/ElementTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Stephen Davies
+ *
+ * This file is part of Eagle2nc.
+ *
+ * Eagle2nc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eagle2nc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Eagle2nc. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.aushacker.eagle2nc.xml;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test Element, not that there is much to actually test.
+ *
+ * @author Stephen Davies
+ * @since October 2018
+ */
+public class ElementTest {
+
+	/**
+	 * Object under test.
+	 */
+	private Element element;
+
+	@Before
+	public void setUp() {
+		element = new Element();
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("Element(null)", element.toString());
+	}
+}
